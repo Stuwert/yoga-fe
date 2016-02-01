0c3d6c9e407a7c2061c1f75f223098bbb4c8c379
Stuart Urback <stuart@urback.net>
2016-02-01 15:02:57 -0700

    Add View Info

--- routes/builder.js
+++ routes/builder.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var results = require('../tmp/results')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('builder');
+  res.render('builder', {results: results.results});
 });
 
 module.exports = router;
