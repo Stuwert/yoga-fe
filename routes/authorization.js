c5ca31f8ca279ad4f85a3675de36c8b6bbe94a62
Josh Silverstein <silverjosh90@gmail.com>
2016-02-01 09:16:46 -0700

    Signup page and Database sync

--- routes/authorization.js
+++ routes/authorization.js
@@ -4,6 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+  res.render('auth/signupLanding')
+
+});
+router.get('/login', function(req, res, next) {
+
   res.render('auth/login')
 
 });
