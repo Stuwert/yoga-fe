39362c5dbde6d4408fb20658679041bf7d23bb4e
Stuart Urback <stuart@urback.net>
2016-02-02 11:06:16 -0700

    Hook up users to builder

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var stooby = require('../tmp/userinfo')
+
 /* GET users listing. */
 router.get('/:username', function(req, res, next) {
-  res.render('user/index', )
+  res.render('user/index', stooby )
 });
 
 //This will need to have a knex call in here to pull up the user information.  Also some authentication to validate it.
---
+++ tmp/userinfo.js
@@ -0,0 +1,29 @@
+module.exports = {
+  name: 'Stuart',
+  user_name: 'stuwert',
+  location: 'Denver, CO',
+  interests: [
+    'hiking',
+    'dogs',
+    'long walks on the beach'
+  ],
+  specialty: 'Vinyasa',
+  sequences: [
+    {
+      'name': 'Sequence 1',
+      'id': 1
+    },
+    {
+      'name': 'Sequence 2',
+      'id': 2
+    }
+  ],
+  favorites: [
+    {
+      creator : 'PatbeamakaGod'
+    },
+    {
+      creator : 'Chris(yogaman)Brown'
+    }
+  ]
+}
