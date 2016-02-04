1f94ba02e4be3b274f8fe43d45ccca149709a13e
Josh Silverstein <silverjosh90@gmail.com>
2016-02-04 15:25:14 -0700

    Pre Ghetto hack

---
+++ serverlogic/serverlogic.js
@@ -0,0 +1,8 @@
+// Misc Variables and functions
+
+
+var yogaStyles = ['Anusara', 'Ashtanga', 'Bikram', 'Hatha', 'Iyengar', 'Jivamukti', 'Kripalu', 'Kundalini', 'Prenatal', 'Restorative', 'Sivananda', 'Viniyoga', 'Yin']
+
+module.exports = {
+  yogaStyles: yogaStyles
+}
