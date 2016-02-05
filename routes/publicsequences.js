4368a8ea19ad7dfe178a901d8e5af7e1e6dac5a8
Stuart Urback <stuart@urback.net>
2016-02-05 10:45:02 -0700

    Public Sequences

--- routes/publicsequences.js
+++ routes/publicsequences.js
@@ -4,19 +4,33 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next){
-
-  res.send("I'm a banana")
+  db.returnPublicSequences(function(sequences){
+    var id = req.isAuthenticated() ? req.user.id : null
+    var name = req.isAuthenticated() ? req.user.first_name + " " + req.user.last_name : null;
+    res.render('sequences/all', {sequences: sequences, user:{
+      exists: req.isAuthenticated(),
+      id: id,
+      name: name
+      }
+    })
+  })
 })
 
 router.get('/:id', function(req, res, next){
   db.returnUserSequence(req.params.id, function(usersequence){
+    var id = req.isAuthenticated() ? req.user.id : null
     if(usersequence.is_public){
-      res.render('sequences/one', usersequence);
+      res.render('sequences/one', {usersequence: usersequence, user: {
+        exists: req.isAuthenticated(),
+        id: id,
+        }
+      });
     }else{
       res.send("This isn't public, sorry bruh.")
     }
   });
 })
+
 
 
 
