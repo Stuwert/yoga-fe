53dbf074ee1d776b6b77b1ac2b0d931ef1c23038
Stuart Urback <stuart@urback.net>
2016-02-05 10:53:57 -0700

    Add save favorites

--- routes/favorites.js
+++ routes/favorites.js
@@ -3,13 +3,23 @@ var router = express.Router();
 var knex = require('../db/knex');
 var db = require('../lib/db_favorites');
 
-router.get('/:user_id/favorites', function(req, res, next){
-  db.get_user_favorites(req.params.user_id, function(result){
-    res.render('user/favorites', {
-      user: req.params.user_id,
-      favorites: result
-    });
-  });
+// router.get('/:user_id/favorites', function(req, res, next){
+//   db.get_user_favorites(req.params.user_id, function(result){
+//     res.render('user/favorites', {
+//       user: req.params.user_id,
+//       favorites: result
+//     });
+//   });
+// })
+
+router.post('/:user_id/favorites', function(req, res, next){
+  var obj = {
+    user_id : req.params.user_id,
+    user_sequence_id : req.body.sequence_id
+  }
+  db.addFavorite(obj, function(){
+    res.redirect('/users/' + req.params.user_id  + '/profile')
+  })
 })
 
 module.exports = router;
