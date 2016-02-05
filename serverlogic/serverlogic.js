ee113ebf56a25d32456af29999667e5d9c979e98
Josh Silverstein <silverjosh90@gmail.com>
2016-02-05 00:34:05 -0700

    added city+state and heroku vars

--- serverlogic/serverlogic.js
+++ serverlogic/serverlogic.js
@@ -3,6 +3,17 @@
 
 var yogaStyles = ['Anusara', 'Ashtanga', 'Bikram', 'Hatha', 'Iyengar', 'Jivamukti', 'Kripalu', 'Kundalini', 'Prenatal', 'Restorative', 'Sivananda', 'Viniyoga', 'Yin']
 
+var states = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI',
+  'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI',
+  'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC',
+  'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
+  'VT', 'VA', 'WA', 'WV', 'WI', 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO',
+  'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY',
+  'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV',
+  'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI',
+  'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ]
+
 module.exports = {
-  yogaStyles: yogaStyles
+  yogaStyles: yogaStyles,
+  states: states
 }
